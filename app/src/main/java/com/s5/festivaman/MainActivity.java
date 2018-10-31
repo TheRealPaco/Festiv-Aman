75a207a1c04cac6eb17d0149f6d4bd1af3db046b
Louise <piel3203@usherbrooke.ca>
2018-10-31 17:38:48 -0500

    nouevelles activitées

--- app/src/main/java/com/s5/festivaman/MainActivity.java
+++ app/src/main/java/com/s5/festivaman/MainActivity.java
@@ -1,7 +1,9 @@
 package com.s5.festivaman;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view)
+    {
+        Intent intent = new Intent(this, accueil.class);
+        startActivity(intent);
+    }
+
+    public void newUser(View view)
+    {
+        Intent intent = new Intent(this, friends.class);
+        startActivity(intent);
     }
 }

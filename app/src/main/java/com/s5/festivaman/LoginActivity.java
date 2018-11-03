1512a024894f78625d9be396932f26f39e4c22ba
Paco <picc2711@usherbrooke.ca>
2018-11-03 13:23:42 -0400

    Add login acticity
    Login activity with scrolling

---
+++ app/src/main/java/com/s5/festivaman/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.s5.festivaman;
+
+import android.arch.lifecycle.ViewModelStoreOwner;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view)
+    {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+
+    public void newUser(View view)
+    {
+        Intent intent = new Intent(this, NewUserActivity.class);
+        startActivity(intent);
+    }
+}

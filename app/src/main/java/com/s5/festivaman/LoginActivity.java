bb11e4639a24afa7708dfc214848f76f8d4dbf4f
Paco <picc2711@usherbrooke.ca>
2018-11-04 16:40:47 -0500

    Clean code
    Clean code and add comments

--- app/src/main/java/com/s5/festivaman/LoginActivity.java
+++ app/src/main/java/com/s5/festivaman/LoginActivity.java
@@ -6,20 +6,34 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     private ProgressBar progressBar;
+    private AlertDialog.Builder loginErrorDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // Change color and hide widget
+        // Prebuild dialog to display possible future error
+        loginErrorDialog = new AlertDialog.Builder(this)
+                                    .setTitle("Login failed")
+                                    .setMessage("Login failed")
+                                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int id) {
+                                        }
+                                    });
+
+        //TODO link keyboard done to connection button
+
+        // Change color and hide progressBar
         progressBar = findViewById(R.id.progressBar);
         progressBar.getIndeterminateDrawable().setColorFilter((int)R.color.colorPrimary,
                 android.graphics.PorterDuff.Mode.MULTIPLY);
@@ -31,25 +45,25 @@ public class LoginActivity extends AppCompatActivity {
         //set progress circle visible till connection is successful or fails
         findViewById(R.id.progressBar).setVisibility(View.VISIBLE);
 
-        if (checkUserPassword()) {
-            // Start Activity
-            Intent intent = new Intent(this, HomeActivity.class);
-            startActivity(intent);
+        try {
+            if (checkUserPassword()) {
+                // Start Activity
+                Intent intent = new Intent(this, HomeActivity.class);
+                startActivity(intent);
 
-            //Close the login activity because it's not needed anymore
-            finish();
-        } else {
-            new AlertDialog.Builder(this)
-                    .setTitle("Login failed")
-                    .setMessage("Wrong username and password")
-                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                        }
-                    })
-                    .show();
+                //Close the login activity because it's not needed anymore
+                finish();
+            } else {
+                // PopUp to display the error
+                loginErrorDialog.setMessage("Wrong username and password")
+                                .show();
 
-            // Hide progress bar, not needed if check was successful since activity would have been killed
-            findViewById(R.id.progressBar).setVisibility(View.GONE);
+                // Hide progress bar, not needed if check was successful since activity would have been killed
+                findViewById(R.id.progressBar).setVisibility(View.GONE);
+            }
+        } catch (Exception e) {
+            loginErrorDialog.setMessage("Error accessing the database")
+                            .show();
         }
     }
 
@@ -68,4 +82,6 @@ public class LoginActivity extends AppCompatActivity {
     private boolean isEditTextEmpty(View view) {
         return ((EditText)view).getText().toString().isEmpty();
     }
+
+
 }

d95c61c88a14a34c65b743922a00324a959e8e65
Paco <picc2711@usherbrooke.ca>
2018-11-05 15:52:27 -0500

    Add splash screen
    Add a splash screen for application cold start

---
+++ app/src/main/java/com/s5/festivaman/AppLoadingActivity.java
@@ -0,0 +1,24 @@
+package com.s5.festivaman;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+public class AppLoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_loading);
+
+        // This activity is only for splash screen so instantly load login activity and close this one
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        this.finish();
+    }
+
+    //TODO CHeck DB status
+
+}

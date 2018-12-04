5b8c267fa0c26ea1b56bd27483d0d60771d32971
Paco <picc2711@usherbrooke.ca>
2018-12-04 10:28:38 -0500

    groupe
    amélioration de l'activité groupe

--- app/src/main/java/com/s5/festivaman/GroupsActivity.java
+++ app/src/main/java/com/s5/festivaman/GroupsActivity.java
@@ -7,8 +7,10 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.s5.festivaman.activities.DrawerActivity;
+import com.s5.festivaman.modifaccount.bracelet;
 
 public class GroupsActivity extends DrawerActivity {
 
@@ -18,4 +20,10 @@ public class GroupsActivity extends DrawerActivity {
         mDrawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
     }
+    public void newGroup(View view)
+    {
+        Intent intent = new Intent(this, NewGroup.class);
+        startActivity(intent);
+    }
+
 }
